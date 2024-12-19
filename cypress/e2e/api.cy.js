af1054ddf6bc3ad783eaac1fec276dc5b1e018b9
Adrian Jiga <jiga.ion.adrian@gmail.com>
2024-12-19 23:33:33 +0200

    expand api spec file with more comprehensive tests

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,13 +1,69 @@
-describe('API Testing with default Cypress', () => {
-  it('fetches todo item successfully', () => {
-    cy.request('GET', 'https://jsonplaceholder.typicode.com/todos/1').then(
-      (response) => {
+describe("DemoQA Book Store API Tests", () => {
+  const apiUrl = "https://demoqa.com/BookStore/v1";
+
+  it("list all the books", { tags: "@api" }, () => {
+    cy.request({
+      method: "GET",
+      url: `${apiUrl}/Books`,
+      headers: {
+        accept: "application/json",
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body.books).to.be.an("array");
+      expect(response.body.books.length).to.be.greaterThan(0);
+
+      response.body.books.forEach((book) => {
+        expect(book).to.include.all.keys(
+          "isbn",
+          "title",
+          "subTitle",
+          "author",
+          "publish_date",
+          "publisher",
+          "pages",
+          "description",
+          "website"
+        );
+      });
+
+      const publishers = new Set(
+        response.body.books.map((book) => book.publisher)
+      );
+      expect(publishers.size).to.eq(2);
+      expect(Array.from(publishers)).to.have.members([
+        "O'Reilly Media",
+        "No Starch Press",
+      ]);
+
+      response.body.books.forEach((book) => {
+        expect(book.pages).to.be.a("number");
+        expect(book.pages).to.be.greaterThan(0);
+      });
+    });
+  });
+
+  it("fetch a specific book by ISBN", { tags: "@api" }, () => {
+    cy.fixture("book").then((expectedBook) => {
+      cy.request({
+        method: "GET",
+        url: `${apiUrl}/Book`,
+        qs: { ISBN: expectedBook.isbn },
+      }).then((response) => {
         expect(response.status).to.eq(200);
-        expect(response.body).to.have.property('userId', 1);
-        expect(response.body).to.have.property('id', 1);
-        expect(response.body).to.have.property('title', 'delectus aut autem');
-        expect(response.body).to.have.property('completed', false);
-      }
-    );
+        expect(response.body).to.deep.include(expectedBook);
+      });
+    });
+  });
+
+  it("handle invalid ISBN", { tags: "@api" }, () => {
+    cy.request({
+      method: "GET",
+      url: `${apiUrl}/Book`,
+      qs: { ISBN: "invalid-isbn" },
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+    });
   });
 });
